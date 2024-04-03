52e17d20e35234413f29db59670ae3157094187d
ankushKun <ankush4singh@gmail.com>
2024-04-04 00:37:17 +0530

    upload to arweave

--- turbotest.py
+++ turbotest.py
@@ -1,17 +1,43 @@
 import arweave
+from arweave.arweave_lib import Wallet, Transaction
+from arweave.transaction_uploader import get_uploader
 
-wallet = arweave.Wallet('wallet.json')
-
-ardrive_url = "https://upload.ardrive.dev"
-
+wallet = Wallet('wallet.json')
 print(wallet.address, wallet.balance)
+
+with open("captures/compressed.png", "rb", buffering=0) as file_handler:
+    print("file opened")
+    tx = Transaction(wallet, file_handler=file_handler,
+                     file_path="captures/compressed.png")
+    # tx.api_url = "https://turbo.ardrive.io"
+    tx.add_tag('Content-Type', 'image/png')
+    tx.add_tag("Type", "image")
+    print("signing")
+    tx.sign()
+    print("signed, price: ", tx.get_price())
+
+    uploader = get_uploader(tx, file_handler)
+
+    print("xyz")
+    while not uploader.is_complete:
+        uploader.upload_chunk()
+
+        print("{}% complete, {}/{}".format(
+            uploader.pct_complete, uploader.uploaded_chunks, uploader.total_chunks
+        ))
+    print("done uploading", tx.id)
+
+
+# ardrive_url = "https://turbo.ardrive.io"
+
+# print(wallet.address, wallet.balance)
 
 #######################
 
 # uploadTxn = arweave.Transaction(
-#     # wallet=wallet, data=open('captures/IMG_20240403_123647.png', "rb").read())
-#     wallet=wallet, data=b'ok')
-# # uploadTxn.api_url = ardrive_url
+#     wallet=wallet, data=open('captures/IMG_20240403_123647.png', "rb").read())
+# wallet=wallet, data=b'ok')
+# uploadTxn.api_url = ardrive_url
 
 # uploadTxn.add_tag("Content-Type", "image/png")
 
@@ -24,9 +50,9 @@ print(wallet.address, wallet.balance)
 
 ####################
 
-tx = arweave.Transaction(
-    wallet, id="UaLQWnAntwORw8xOkDYDV4J6kRSXi0L3707iFzbi3CxfLQ5ogLlLRoujUOlWAEKN")
-print(tx.data)
-print(tx.get_status())
+# tx = arweave.Transaction(
+#     wallet, id="UaLQWnAntwORw8xOkDYDV4J6kRSXi0L3707iFzbi3CxfLQ5ogLlLRoujUOlWAEKN")
+# print(tx.data)
+# print(tx.get_status())
 
 # UaLQWnAntwORw8xOkDYDV4J6kRSXi0L3707iFzbi3CxfLQ5ogLlLRoujUOlWAEKN
